25ba36e67c0dd62582a2f3cb69097ed3fb730725
orumin <dev@orum.in>
2021-02-04 22:05:33 +0900

    fix script

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-sed -i -e 's/^Options=\(.*\)$/Options=\/HAL=halpc98.dll \1/g' boot/bootdata/livecd.ini
+if [ -z `grep halpc98 boot/bootdata/livecd.ini` ]; then
+    sed -i -e 's/^Options=\(.*\)$/Options=\/HAL=halpc98.dll \1/g' boot/bootdata/livecd.ini
+fi
 ./configure.sh -DSARCH=pc98
 cd output-MinGW-i386
 ninja pc98bootfdd
